73cf37542835a84f4bfa31c02bcd198b964dabba
Lillian Paris <lillian.paris529@gmail.com>
2022-02-02 10:18:27 -0700

    testig netlify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Calc from "./Pages/landing/landing";
+import ParticlesBg from 'particles-bg'
 
 
 function App() {
 
     return (
       <>
-    
+     <ParticlesBg type="circle" bg={true} />
         <Calc />
       </>
     );
